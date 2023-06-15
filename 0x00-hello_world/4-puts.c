03f746b3d262ed962a129768859239de1889499c
laribox <laribox2014@gmail.com>
2023-06-15 01:27:55 -0700

    script that compiles a C file and creates an executable named cisfun

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,10 @@
 
 /**
  * main - print a simple line to stdout
+ * Return: 0 to exit the program
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
