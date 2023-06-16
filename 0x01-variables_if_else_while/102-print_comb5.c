e7f89f5e752bf40fcb0e21d2acca73151543fd4a
laribox <laribox2014@gmail.com>
2023-06-16 10:33:05 -0700

    program that prints all possible combinations of two two-digit numbers.

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet letter with putchar
+ * Return: 0 to exit
+ */
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 48 ; i <= 57; i++)
+	{
+		for (j = 48 ; j <= 57; j++)
+		{
+			for (k = 48 ; k <= 57 ; k++)
+			{
+				for (l = 48 ; l <= 57 ; l++)
+				{
+					if (k + l != i + j && i + j < l + k)
+					{
+						putchar(i);
+						putchar(j);
+						putchar(' ');
+						putchar(k);
+						putchar(l);
+					}
+					if (k + l != i + j && i + j < l + k && l + k + i + j != 227)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
