c16462074de5c1f8ed84394038852224c8714a06
laribox <laribox2014@gmail.com>
2023-06-20 06:15:52 -0700

    function that prints the 9 times table, starting with 0.

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,16 +16,22 @@ void times_table(void)
 			if ((i * j) < 10)
 			{
 				_putchar((i * j) + '0');
+				if (j != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
 			}
 			else
-			{	
+			{
 				_putchar(((i * j) / 10) + '0');
 				_putchar(((i * j) % 10) + '0');
-			}
-			if (j != 9)
-			{
-				_putchar(',');
-				_putchar(' ');
+				if (j != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
 		}
 		_putchar('\n');
