ac41dd4706edde039d173c77dc749ac7db3243e0
laribox <laribox2014@gmail.com>
2023-06-20 14:55:25 -0700

    prints the first 98 Fibonacci numbers, starting with 1 and 2, followed by a new line.

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,11 +12,11 @@ int main(void)
 	i = 1;
 	j = 2;
 	printf("%lu, %lu, ", i, j);
-	for (x = 1 ; x <= 97 ; x++)
+	for (x = 1 ; x <= 96 ; x++)
 	{
 		k = i + j;
 		printf("%lu", i + j);
-		if (x != 97)
+		if (x != 96)
 		{
 			printf(", ");
 		}
