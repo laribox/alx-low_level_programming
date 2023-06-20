7097fc4f24ce38bf1428eea5112fc556c8b40b74
laribox <laribox2014@gmail.com>
2023-06-20 08:51:00 -0700

    function that prints all natural numbers from n to 98, followed by a new line.

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,42 +14,28 @@ void print_to_98(int n)
 	{
 		for (x = n ; x <= 98 ; x++)
 		{
-			if (x > 9 || x < -9)
+			if (x < 0)
 			{
-				if (x < 0)
-				{
-					_putchar('-');
-					y = x * -1;
-				}
-				else
-					y = x;
+				_putchar('-');
+				y = x * -1;
+			}
+			else
+				y = x;
+			if (y > 9 && y < 100)
+			{
 				_putchar((y / 10) + '0');
 				_putchar(y % 10 + '0');
 
 			}
-			else if (x > 99 || x < -99)
+			else if (y > 99)
 			{
-				if (x < 0)
-				{
-					_putchar('-');
-					y = x * -1;
-				}
-				else
-					y = x;
-				_putchar(y / 100 + '0');
-				_putchar(y / 10 + '0');
-				_putchar(y % 10 + '0');
+				_putchar((y / 100) + '0');
+				_putchar((y / 10) % 10 + '0');
+				_putchar((y % 10) + '0');
 
 			}
 			else
 			{
-				if (x < 0)
-				{
-					_putchar('-');
-					y = x * -1;
-				}
-				else
-					y = x;
 				_putchar(y + '0');
 			}
 
@@ -62,12 +48,9 @@ void print_to_98(int n)
 	}
 	else if (n > 98)
 	{
-		_putchar('+');
-		_putchar('+');
-		_putchar('+');
 		for (x = n; x <= 98; x--)
 		{
-			if (x > 9)
+			if (x > 9 && x < 100)
 			{
 				_putchar(x / 10 + '0');
 				_putchar(x % 10 + '0');
@@ -76,7 +59,7 @@ void print_to_98(int n)
 			else if (x > 99)
 			{
 				_putchar(x / 100 + '0');
-				_putchar(x / 10 + '0');
+				_putchar((x / 10) % 10 + '0');
 				_putchar(x % 10 + '0');
 
 			}
@@ -94,29 +77,36 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (x = n; x <= 98 - n ; x++)
+		if (n == 98)
 		{
-			if (x > 9)
+			_putchar('9');
+			_putchar('8');				
+		}
+		else
+		{
+			for (x = n; x <= 98 - n ; x++)
 			{
-				_putchar(x / 10 + '0');
-				_putchar(x % 10 + '0');
+				if (x > 9)
+				{
+					_putchar(x / 10 + '0');
+					_putchar(x % 10 + '0');
 
-			}
-			else if (x > 99)
-			{
-				_putchar(x / 100 + '0');
-				_putchar(x / 10 + '0');
-				_putchar(x % 10 + '0');
-
-			}
-			else
-			{
-				_putchar(x + '0');
-			}
-			if (x != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
+				}
+				else if (x > 99)
+				{
+					_putchar(x / 100 + '0');
+					_putchar(x / 10 + '0');
+					_putchar(x % 10 + '0');
+				}
+				else
+				{
+					_putchar(x + '0');
+				}
+				if (x != 98)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
 		}
 	}
