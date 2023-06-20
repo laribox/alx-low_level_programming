764d7859c9a22f0a41237bd85c155b585ce607fe
laribox <laribox2014@gmail.com>
2023-06-20 05:20:59 -0700

    function that prints the last digit of a number.

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+int print_last_digit(int);
+/**
+ * print_last_digit - get value of last digit
+ *@c: character to check
+ * Return: value of last digit
+ */
+int print_last_digit(int c)
+{
+	return (c % 10);
+}
