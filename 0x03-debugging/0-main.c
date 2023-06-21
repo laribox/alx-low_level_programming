957ccdee0175a7c1176a6d9d73c39ffc2a4fa24d
laribox <laribox2014@gmail.com>
2023-06-21 06:44:18 -0700

    fix

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,17 +5,13 @@
 * Return: 0
 */
 
-int main(void)
+void positive_or_negative(int i)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n < 0)
-		printf("%d is negative\n", n);
-	else if (n > 0)
-		printf("%d is positive\n", n);
+	if (i < 0)
+		printf("%d is negative\n", i);
+	else if (i > 0)
+		printf("%d is positive\n", i);
 	else
-		printf("%d is zero\n", n);
+		printf("%d is zero\n", i);
 	return (0);
 }
