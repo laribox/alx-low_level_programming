c438d3aa9418ababb5326a678b2270b3996d66e1
laribox <laribox2014@gmail.com>
2023-06-21 06:46:20 -0700

    fix

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
+* positive_or_negative - tests function that
+* prints if integer is positive or negative
+* @i: number to check
 */
 
 void positive_or_negative(int i)
