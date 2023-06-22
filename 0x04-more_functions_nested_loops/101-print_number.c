21feae7777691b6e98a5a670970cd3a5ff4955e3
laribox <laribox2014@gmail.com>
2023-06-22 08:49:03 -0700

    function that prints an integer. with putchar

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,45 +1,26 @@
 #include "main.h"
+
 /**
- * print_number - print a number
- * @n: number to print
+ * print_number - Print an integer.
+ * @n: Integer that need to be printed.
  */
 void print_number(int n)
 {
-	if (n < 0)
+	 int temp = 0;
+
+	if (n >= 0)
 	{
-		n = n * -1;
+		temp = n;
+	}
+	else
+	{
+		temp = -n;
 		_putchar('-');
 	}
-	if (n > 999999999) {
-		printDigit((n / 10000000000) % 10);
+	if (temp / 10 > 0)
+	{
+		print_number(temp / 10);
 	}
-	if (n > 99999999) {
-		printDigit((n / 100000000) % 10);
-	}
-	if (n > 9999999) {
-		printDigit((n / 10000000) % 10);
-	}
-	if (n > 999999) {
-		printDigit((n / 1000000) % 10);
-	}
-	if (n > 99999) {
-		printDigit((n / 100000) % 10);
-	}
-	if (n > 9999) {
-		printDigit((n / 10000) % 10);
-	}
-	if (n > 999) {
-		printDigit((n / 1000) % 10);
-	}
-	if (n > 99) {
-		printDigit((n / 100) % 10);
-	}
-	if (n > 9) {
-		printDigit((n / 10) % 10);
-	}
-	printDigit(n % 10);
+	_putchar((temp % 10) + 48);
 }
 
-void printDigit(int digit) {
-	_putchar(digit + '0');
-}
