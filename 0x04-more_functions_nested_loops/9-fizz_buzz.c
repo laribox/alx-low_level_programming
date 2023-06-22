53a0f14c6e219b745b227d3350878577f36ae741
laribox <laribox2014@gmail.com>
2023-06-22 16:23:51 -0700

    Fizz-Buzz improved

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,16 +9,17 @@ int main(void)
 
 	for (i = 1 ; i <= 100 ; i++)
 	{
-			if ((i % 3) == 0)
-				printf("Fizz");
-			if ((i % 5) == 0)
-				printf("Buzz");
-			if (i != 100)
-			printf(" ");
+		if ((i % 3) == 0)
+			printf("Fizz");
+		if ((i % 5) == 0)
+			printf("Buzz");
 		if ((i % 3 != 0) && (i % 5 != 0))		
 		{
-			printf("%d ", i);
+			printf("%d", i);
 		}
+
+		if (i != 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
