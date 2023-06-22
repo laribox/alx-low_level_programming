843fca737150068731c1238c4d81179990c87c83
laribox <laribox2014@gmail.com>
2023-06-22 15:58:07 -0700

    im done ++

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -10,7 +10,7 @@ int main(void)
 	unsigned long int i, x;
 
 	x = 612852475143;
-	for (i = 3; i < 782849; i = i + 2)
+	for (i = 3; i < x; i = i + 2)
 	{
 		while ((x % i == 0) && (x != i))
 			x = x / i;
