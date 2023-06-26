4db7bde1a50cc415926fbe285bcb50e4b57a0fda
laribox <laribox2014@gmail.com>
2023-06-26 03:32:12 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string followed by a new line to stdout
+ * @str: Pointer to string.
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; i < strlen(str); ++i);
+	{
+		_putchar(str[i] + '0');
+	}
+	_putchar('\n');
+}
