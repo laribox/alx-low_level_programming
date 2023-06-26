877e1a2a8d8a6fde330b6a2f60d62823ac7ee5ae
laribox <laribox2014@gmail.com>
2023-06-26 03:23:45 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @s: param pointer of char type.
+ *
+ * Return: Length of a string.
+ */
+int _strlen(char *s)
+{
+	return strlen(*s);
+}
