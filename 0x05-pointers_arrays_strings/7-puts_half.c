d100aeddf257550f5f12ed90b4c142c6917b3ac0
laribox <laribox2014@gmail.com>
2023-06-26 04:21:03 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,9 +5,13 @@
  */
 void puts_half(char *str)
 {
-	int i;
+	int i,x;
 
-	for (i = ((int)strlen(str) / 2); i < (int)strlen(str) - 1; ++i)
+	if (strlen(s) % 2 == 0)
+		x = 0;
+	else
+		x = 1;
+	for (i = ((int)strlen(str) / 2) - x; i < (int)strlen(str); ++i)
 	{
 		_putchar(str[i]);
 	}
