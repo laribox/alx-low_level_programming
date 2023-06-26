391a1150e530b9c7b5f6b70b254072629b4c1aea
laribox <laribox2014@gmail.com>
2023-06-26 03:46:55 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string.
+ * @s: Pointer to string.
+ */
+void rev_string(char *s)
+{
+	int i,t;
+
+	for (i = 0; i < (int)strlen(s) / 2; ++i)
+	{
+		t = s[strlen(s) - i];
+		s[strlen(s) - i] = s[i];
+		s[i] = t;		
+	}
+}
