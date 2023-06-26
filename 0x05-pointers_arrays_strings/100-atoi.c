1099bdfc3a7f2904c21f4094faf1440001670f00
laribox <laribox2014@gmail.com>
2023-06-26 09:25:01 -0700

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,5 +7,23 @@
  */
 int _atoi(char *s)
 {
-	return (atoi(s));
+	int i, j = 0, counter = 0;
+	char string[5];
+
+	for (i = 0 ; i < (int)strlen(s) ; ++i)
+	{
+		if (s[i] == '-')
+		{
+			++counter;
+		}
+		if (s[i] >= 48 && s[i] <= 57)
+		{
+			string[j] = s[i];
+			++j;
+		}
+	}
+	if (counter % 2 != 0)
+		return (atoi(string) * -1);
+	else
+		return (atoi(string));
 }
