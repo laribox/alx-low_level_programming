6b3ff13fa3f7a1c6d90ff352cd33d141abaf9ebc
laribox <laribox2014@gmail.com>
2023-06-26 06:01:41 -0700

    task 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _atoi - Convert a string to an integer.
+ * @s: The pointer to convert from string to int
+ * Return: A integer
+ */
+int _atoi(char *s)
+{
+	return (atoi(s));
+}
