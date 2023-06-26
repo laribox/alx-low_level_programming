4416aa73096a0e384eabd55a0a5c20e3f151274a
laribox <laribox2014@gmail.com>
2023-06-26 03:40:48 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string, in reverse, followed by a new line.
+ * @s: String pointer.
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = (int)strlen(s) - 1 ; i >= 0 ; --i)
+	{
+		_putchar(s[i]);
+	}
+}
