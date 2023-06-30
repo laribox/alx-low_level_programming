cb1c19de4232b841ee2bd24a701541937d0b48b8
laribox <laribox2014@gmail.com>
2023-06-29 18:03:29 -0700

    task  3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,7 @@
  * _strcmp - function that compare two strings.
  * @s1: string
  * @s2: string
- * Return: 0 if strings are equal 
+ * Return: 0 if strings are equal
  */
 
 int _strcmp(char *s1, char *s2)
