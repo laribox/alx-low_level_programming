158f31b1c56fc89335f4731c88456dd17b3241c6
laribox <laribox2014@gmail.com>
2023-06-29 17:57:59 -0700

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,7 +8,7 @@
  * Return: a pointer to the resulting string dest
  */
 
-char _strncpy(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	strncpy(dest, src, n);
 	return (dest);
