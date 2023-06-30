6f3d30481d423cc54f0e2e4c63345503540f0188
laribox <laribox2014@gmail.com>
2023-06-29 18:57:55 -0700

    task  6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalizes all words of a string.
+ * @str: string pointer.
+ *
+ * Return: Pointer to the string that has been changed.
+ */
+char *cap_string(char *str)
+{
+	return (str);
+}
