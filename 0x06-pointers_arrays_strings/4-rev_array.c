167f1fa235ddcf2d47ead9540de781b486f82bce
laribox <laribox2014@gmail.com>
2023-06-29 18:17:33 -0700

    task  4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reverse_array - function that reverses the content of an 
+ * reverse_array - function that reverses the content of an
  *	           array of integers.
  * @a: array
  * @n: array number of elements
