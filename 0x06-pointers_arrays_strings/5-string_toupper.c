c43e935772685c02694f61127795d6b602890137
laribox <laribox2014@gmail.com>
2023-06-29 18:37:54 -0700

    task  5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -12,7 +12,7 @@ char *string_toupper(char *a)
 	int i;
 
 	for (i = 0; i < strlen(a) ; i++)
-	{	if (a >= 'a' && a <= 'z')
+	{	if (a[i] >= 'a' && a[i] <= 'z')
 			a[i] = a[i] - 32;
 	}
 	return (a);
