90a58a791b48658257354c7e3153ce192db42451
laribox <laribox2014@gmail.com>
2023-06-29 19:21:21 -0700

    task  6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,3 @@
-#include "main.h"
-
 /**
  * cap_string - Capitalizes all words of a string.
  * @str: string pointer.
@@ -9,17 +7,21 @@
 char *cap_string(char *str)
 {
 	int i;
-	
+
 	for (i = 0; i < (int)strlen(str) ; i++)
 	{
-		if ((str[i] > 32 && str[i] < 48) ||
-		(str[i] > 57 && str[i] < 64) || str[i] > 122 ||
-		str[i] == " " || str[i] == '\t' || str[i] == '\n')
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+				str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+				str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+				str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+				str[i] == '}')
 		{
 			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-				str[i] -= 32;
+			{
+				str[i + 1] -= 32;
+			}
 		}
-		if (str[i + 1] >= 'a' && str[i + 1] <= 'z' && i == 0)
+		if (str[i] >= 'a' && str[i] <= 'z' && i == 0)
 			str[i] -= 32;
 	}
 	return (str);
