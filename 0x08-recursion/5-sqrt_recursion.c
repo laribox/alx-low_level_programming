4b6690264b39f9bc65c8aa68b047758fd2f38c7d
laribox <laribox2014@gmail.com>
2023-07-05 00:37:36 -0700

    task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+/**
+ * _sqrt_recursion - find natural square root of a number
+ * @n: Integer to check
+ * Return: natural square root or -1
+ */
+int _sqrt_recursion(int n)
+{
+	helper_function(1, n);
+
+}
+
+/**
+ * helper_function - help to find if there is a root number
+ * @x: Incremental value to reach the square root
+ * @y: value of n
+ * Return: Root or -1
+ */
+
+int helper_function(int x, int y)
+{
+	if (y == x * x)
+	{
+		return (x);
+	}
+	else if (y < x * x)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (helper_function(x + 1, y));
+	}
+}
