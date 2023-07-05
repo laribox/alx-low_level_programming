bf25fab09d413e09426737d7e5ea1767bc9d301d
laribox <laribox2014@gmail.com>
2023-07-04 22:40:17 -0700

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints a string, followed by a new line
+ * @s: String to be printed
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+	if (s != '\0')
+	{
+		_putchar(s);
+		s++;
+		_put_recursion(s);
+	}
+}
