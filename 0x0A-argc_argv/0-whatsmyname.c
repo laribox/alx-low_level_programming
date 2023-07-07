ddb7413f555be30b890aef0a79d358d4233dd8b3
laribox <laribox2014@gmail.com>
2023-07-06 20:21:46 -0700

    initializing the tasks

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - main function
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: 0 always
+ */
+
+int main (int argc, char *argv[])
+{
+	printf("%s\n",argv[0]);
+	return (0);
+}
