6a4a14e952cd6e3a34653ff2eb559f6432726eb5
laribox <laribox2014@gmail.com>
2023-07-06 21:20:12 -0700

    last task

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,71 @@
+#include "main.h"
+int getChange(int money);
+/**
+ * main - Function prints minimum number of coins to
+ * to make change
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: 0 always
+ */
+
+int main(int argc, char *argv[])
+{
+	int money;
+
+	money = atoi(argv[1]);
+	if (money < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	if (argc == 2)
+	{
+		printf("%d\n", getChange(money));
+	}
+	else
+		printf("Error\n");
+	return (1);
+
+	return (0);
+}
+
+/**
+ * getChange - get the change
+ * @money: money to get change for
+ * Return: number of coins
+ */
+int getChange(int money)
+{
+	int coins = 0;
+
+	while (money)
+	{
+		if (money >= 25)
+		{
+			coins++;
+			money -= 25;
+		}
+		else if (money >= 10)
+		{
+			coins++;
+			money -= 10;
+		}
+		else if (money >= 5)
+		{
+			coins++;
+			money -= 5;
+		}
+		else if (money >= 2)
+		{
+			coins++;
+			money -= 2;
+		}
+		else if (money >= 1)
+		{
+			coins++;
+			money -= 1;
+		}
+	}
+	return (coins);
+}
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * main - Function that sums the positive arguments
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: 0 always
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum = 0, i;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc ; i++)
+		{
+			if (atoi(argv[i]))
+			{
+				sum += atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", sum);
+	}
+	else
+		printf("0\n");
+
+	return (0);
+}
