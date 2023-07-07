c1b07dcf89c0c9e25b92e81ed51b1aa57c430152
laribox <laribox2014@gmail.com>
2023-07-07 02:13:31 -0700

    test

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,15 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+#define TRUE  1
+#define FALSE 0
+
 #include <unistd.h>
 #include <stdio.h>
-#include <string.h>
 
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
-int _abs(int n);
+int _abs(int);
 int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
-void _puts(char *s);
+void _puts(char *str);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
@@ -22,3 +26,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+#endif /* for MAIN_H */
