dd6f1dae168c6e0ff90e3308ef5145ce08109b05
laribox <laribox2014@gmail.com>
2023-07-10 19:14:10 -0700

    task 5

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,46 @@
+/**
+ * argstostr - Function  function that concatenates all
+ * the arguments of your program.
+ * @ac: Number of argumrnts
+ * @av: Array of arguments
+ * Return: String pointer
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i, len = 0, temp = 0;
+	char *result;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac ; i++)
+	{
+		temp = 0;
+		while (av[i][temp] != '\0')
+		{
+			temp++;
+			len++;
+		}
+		len++;
+	}
+	result = malloc(sizeof(char) * (len + 1));
+	if (result == NULL)
+		return (NULL);
+
+	len = 0;
+	for (i = 0; i < ac ; i++)
+	{
+		temp = 0;
+		while (av[i][temp] != '\0')
+		{
+			result[len] = av[i][temp];
+			temp++;
+			len++;
+		}
+
+		result[len++] = '\n';
+	}
+	result[len] = '\0';
+
+	return (result);
+}
