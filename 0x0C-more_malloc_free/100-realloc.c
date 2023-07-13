93d9697c92f0dbc362b9e3aaa6c0dc33eb4b003d
laribox <laribox2014@gmail.com>
2023-07-13 11:20:56 -0700

    task 4

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * array_range - Function that reallocates a memory 
+ * block using malloc and free
+ * @ptr: pointer to the memory previously allocated
+ * @old_size: Old mem size
+ * @new_size: New mem size
+ * Return: Array pointer
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *mem;
+	unsigned int x = new_size;
+
+	if (ptr == NULL)
+	{
+		mem = malloc(new_size);
+		return (mem);
+	}
+	else if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	else if (new_size == new_size)
+	{
+		return ptr;
+	}
+
+	mem = malloc(new_size);
+	if (new_size > old_size)
+	{
+		x = old_size;
+	}
+
+	strncpy(mem, ptr, x);
+	free(ptr);
+	return (mem);
+
+}
