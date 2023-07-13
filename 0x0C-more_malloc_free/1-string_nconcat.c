57935c4d00b5b049af815bec6694a82355bae59c
laribox <laribox2014@gmail.com>
2023-07-13 08:41:11 -0700

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,14 +13,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	int i = 0, j;
 	char *conc;
 
-	if (*s1 == NULL)
+	if (s1 == NULL)
 	{
-		*s1 = "";
+		s1 = "";
 	}
 
-	if (*s2 == NULL)
+	if (s2 == NULL)
 	{
-		*s2 = "";
+		s2 = "";
 	}
 
 	if (n >= strlen(s2))
