c769726bf4f538a967f652a850722c946a92235c
laribox <laribox2014@gmail.com>
2023-07-17 16:07:53 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * dog - dog struct
+ * @name: name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ */
+
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;	
+}
