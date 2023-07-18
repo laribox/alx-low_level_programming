4e4b6e491e78fd478897a6c7d6f1837301cf04bb
laribox <laribox2014@gmail.com>
2023-07-17 17:00:39 -0700

    task 0 modified

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
  * struct dog - dog struct
  * @name: name of the dog
@@ -14,6 +15,8 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-}
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
