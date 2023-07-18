e14acb87495eaef1b92914d7b4d0607912d7e2ea
laribox <laribox2014@gmail.com>
2023-07-17 17:03:09 -0700

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes a struct of dog
+ * @d: pointer to struct
+ * @name: pointer to name of the dog
+ * @age: The dog age 
+ * @owner: owner of dog pointer
+ *
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
