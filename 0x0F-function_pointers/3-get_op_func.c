5efae3e0c4f36df7c29aeedd5034f626302958d8
laribox <laribox2014@gmail.com>
2023-07-19 11:16:31 -0700

    task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -16,13 +16,13 @@ int (*get_op_func(char *s))(int, int)
 	{ "%", op_mod },
 	{ NULL, NULL }
 	};
-	int i ;
+	int i;
 
-	for (i = 0;i < 5; i++)
+	while (i < 5)
 	{
 		if (strcmp(s, ops[i].op) == 0)
 			return (ops[i].f);
-
+		i++;
 	}
 
 	return (0);
