9fc719c37d12ef79b7c23fa679b04e65756591ef
laribox <laribox2014@gmail.com>
2023-07-19 10:19:29 -0700

    task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+  * int_index - function that search for value
+  * in an array
+  * @array: ...
+  * @size: Size of the array
+  * @cmp: function to search 
+  *
+  * Return: ...
+  */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size > 0 && array && cmp)
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+			{
+				return (i);
+			}
+		}
+
+	return (-1);
+}
