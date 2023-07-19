1a774fbcdf086694b3f62e1603b8fe9159d53644
laribox <laribox2014@gmail.com>
2023-07-19 11:00:58 -0700

    task 3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -14,9 +14,19 @@ int op_mul(int a, int b)
 }
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a / b);
 }
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
