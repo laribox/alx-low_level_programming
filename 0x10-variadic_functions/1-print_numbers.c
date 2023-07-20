736c07304634df9a1dc20d15ad53c0630ec5ac14
laribox <laribox2014@gmail.com>
2023-07-20 10:24:11 -0700

    task 1 fix

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,9 +15,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(ap, n);
 	for (i = 0; i < n ; i++)
 	{
-		printf("%d",va_arg(ap, int));
+		printf("%d", va_arg(ap, int));
 		if (i < n - 1 && separator != NULL)
-			printf("%s",separator);
+			printf("%s", separator);
 	}
 
 	printf("\n");
