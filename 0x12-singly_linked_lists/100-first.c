fcc27a89b0208526822bf6d068d778aa3b7be3b1
laribox <laribox2014@gmail.com>
2023-07-27 09:12:21 -0700

    task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
-// The function you want to execute before main
 void my_function(void) __attribute__((constructor));
 
-void my_function(void) {
-    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+
+/**
+ * my_function - function run before main
+ */
+void my_function(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
