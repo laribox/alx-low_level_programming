cef271f24cf64deb70e90b855089e2a74dbcae1b
laribox <laribox2014@gmail.com>
2023-07-27 08:54:20 -0700

    task 3

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,47 @@
+/**
+ * add_node_end - adds a new node to the end of linked list
+ * @head: double pointer to a linked list
+ * @str: string to add to the new node
+ *
+ * Return: pointer to the new node
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new, *tmp;
+
+	*head = tmp;
+	if (str == NULL)
+		return (NULL);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->len = _strlen(new->str);
+	new->next = NULL;
+
+	while (tmp->next)
+	{
+		tmp = tmp->next;
+	}
+	tmp->next = new;
+
+	return (new);
+}
+
+/**
+ * _strlen - Calculates the length of a string.
+ * @str: String to count.
+ *
+ * Return: Length of the string.
+ */
+unsigned int _strlen(const char *str)
+{
+	if (!*str)
+		return (0);
+	return (1 + _strlen(str + 1));
+}
