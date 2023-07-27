dffb1c082017a59a5f403c802da992c7c36a80ef
laribox <laribox2014@gmail.com>
2023-07-27 08:18:40 -0700

    task 0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,3 +1,6 @@
+#ifndef LISTS
+#define LISTS
+
 #include <stdio.h>
 #include <stdarg.h>
 #include <stddef.h>
@@ -28,3 +31,5 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 
 void free_list(list_t *head);
+
+#endif
