e075c3d64002d943ea5d57b25b759c4a03cacb6b
laribox <laribox2014@gmail.com>
2023-07-27 08:40:21 -0700

    task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,7 +10,7 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_head;
 
-	new_head = malloc(size_of(list_t));
+	new_head = malloc(sizeof(list_t));
 	if (new_head == NULL)
 		return (NULL);
 
