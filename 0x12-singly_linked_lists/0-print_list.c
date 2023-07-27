d8c1a9b355998ef3de845f8590cdc98609df9150
laribox <laribox2014@gmail.com>
2023-07-27 08:14:58 -0700

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_list - print the node elements
+ * @h: name of the list
+ * Return: the number of nodes.
+ */
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
