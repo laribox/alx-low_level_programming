c59449017a04ee996f74c3edee78ffcd54eaac07
laribox <laribox2014@gmail.com>
2023-07-27 09:16:59 -0700

    task 6

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,20 @@
+section .data
+    hello db 'Hello, Holberton', 0
+    format db '%s', 10, 0 
+
+section .text
+    global main
+    extern printf
+
+main:
+    push hello
+
+    push format
+
+    call printf
+
+    add rsp, 16
+
+    xor rdi, rdi
+    ret
+
