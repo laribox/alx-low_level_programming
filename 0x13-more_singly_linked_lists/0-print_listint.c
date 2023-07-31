2b5fd156079ad1037135726aae7fdcf80194fd2b
laribox <laribox2014@gmail.com>
2023-07-31 14:01:02 -0700

    task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_listint - print the node elements
+ * @h: name of the list
+ * Return: the number of nodes.
+ */
+size_t print_listint(const listint_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+			printf("%d\n",h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
