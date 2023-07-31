de25d595737eee26cfa5517881ea7f0bad08ed45
laribox <laribox2014@gmail.com>
2023-07-31 14:03:12 -0700

    task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,17 +1,16 @@
 #include "lists.h"
 
 /**
- * print_listint - print the node elements
+ * listint_len - get the length of list
  * @h: name of the list
  * Return: the number of nodes.
  */
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	int count = 0;
 
 	while (h)
 	{
-		printf("%d\n", h->n);
 		count++;
 		h = h->next;
 	}
