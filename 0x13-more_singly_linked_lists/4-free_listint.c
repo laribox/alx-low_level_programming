357e088f9a054018e7d8c5f91dcf0ca96704b079
laribox <laribox2014@gmail.com>
2023-07-31 14:49:47 -0700

    task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * free_listint - Realease the memory allocated for a list
+ * @head: A pointer to the first node of the list to free
+ */
+void free_listint(listint_t *head)
+{
+	if (head)
+	{
+		free_list(head->next);
+		free(head);
+	}
+}
