288563f8e173a4b8a1503ef65053ebcee4080003
laribox <laribox2014@gmail.com>
2023-07-31 15:08:51 -0700

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Realease the memory allocated for a list
+ * @head: A pointer to the first node of the list to free
+ */
+void free_listint2(listint_t **head)
+{
+  listint_t *next_val;
+
+  if (head == NULL)
+	  return;
+
+
+  while (*head)
+  {
+    next_val = (*head)->next;
+    free(*head);
+    *head = next_val;
+  }
+}
