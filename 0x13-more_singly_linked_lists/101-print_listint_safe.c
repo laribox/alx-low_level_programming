5960ce2fc2e54ec8b4a3cefeb65e30013501529d
laribox <laribox2014@gmail.com>
2023-07-31 17:43:07 -0700

    task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,18 +10,21 @@ size_t print_listint_safe(const listint_t *head)
 	int count = 0;
 	const listint_t *new_head = head;
 
-  if (head == NULL)
-    exit(98);
+	if (head == NULL)
+		exit(98);
 
-	while (head)
+	while (head && new_head && new_head->next)
 	{
-		if (count > 0 && new_head == head)
+		printf("[%p] %d\n", (void *)head, head->n);
+		count++;
+
+		head = head->next;
+		new_head = new_head->next->next;
+
+		if (new_head == head)
 		{
 			exit(98);
 		}
-		printf("[%p] %d\n",(void *)head, head->n);
-		count++;
-		head = head->next;
 	}
 	return (count);
 }
