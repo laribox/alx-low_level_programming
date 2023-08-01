061d549386f031b8cae110acf02696aa2c0d764c
laribox <laribox2014@gmail.com>
2023-07-31 18:13:19 -0700

    task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,30 +1,39 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - print the node elements safely
- * @head: name of the list
- * Return: the number of nodes.
+ * print_listint_safe - Prints a listint_t linked list
+ * @head: Pointer to the head of the list
+ *
+ * Return: The number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	int count = 0;
-	const listint_t *new_head = head;
+	const listint_t *slow, *fast;
+	size_t count = 0;
 
-	if (head == NULL)
-		exit(98);
+	slow = head;
+	fast = head;
 
-	while (head && new_head && new_head->next)
+	while (slow != NULL && fast != NULL && fast->next != NULL)
 	{
-		printf("[%p] %d\n", (void *)head, head->n);
-		count++;
+		slow = slow->next;
+		fast = fast->next->next;
 
-		head = head->next;
-		new_head = new_head->next->next;
-
-		if (new_head == head)
+		/* If a loop is detected, exit the program with status 98 */
+		if (slow == fast)
 		{
+			printf("[%p] %d\n", (void *)slow, slow->n);
+			printf("-> [%p] %d\n", (void *)fast->next, fast->next->n);
 			exit(98);
 		}
 	}
-	return (count);
+
+	while (head != NULL)
+	{
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		count++;
+	}
+
+	return count;
 }
