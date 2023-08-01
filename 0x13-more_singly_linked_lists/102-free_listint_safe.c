c25a8c3e58e9077656dbc227f7822a15d611a8b1
laribox <laribox2014@gmail.com>
2023-07-31 18:05:16 -0700

    task 13

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -3,19 +3,39 @@
 /**
  * free_listint_safe - Realease the memory allocated for a list
  * @h: A pointer to the first node of the list to free
+ * Return: size of how many freed nodes
  */
 size_t free_listint_safe(listint_t **h)
 {
 	listint_t *next_val;
+	size_t len = 0;
+	int diff;
 
-	if (head == NULL)
-		return;
+	if (*h == NULL || h == NULL)
+		return (0);
 
 
 	while (*h)
 	{
-		next_val = (*h)->next;
-		free(*h);
-		*h = next_val;
+		diff = *h - (*h)->next;
+		if (diff > 0)
+		{
+			next_val = (*h)->next;
+			free(*h);
+			*h = next_val;
+			len++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			len++;
+			break;
+		}
+
 	}
+
+	*h = NULL;
+
+	return (len);
 }
