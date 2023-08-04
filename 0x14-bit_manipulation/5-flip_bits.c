65bfe4694412a35f71afb05c7571b01f4b9ac2ca
laribox <laribox2014@gmail.com>
2023-08-03 17:23:34 -0700

    task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * flip_bits - Gets the number of bits to be
+ *		flipped to convert n to m.
+ * @n: First number.
+ * @m: Second number.
+ * Return: Number of bits to be flipped to make n into m.
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int i, counter = 0;
+
+	i = n ^ m;
+	while (i > 0)
+	{
+		counter++;
+		i = i >> 1;
+	}
+	return (counter);
+}
