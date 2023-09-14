1890942ef3cab59ab61574ab68a275a66a55bd43
laribox <laribox2014@gmail.com>
2023-09-14 22:23:21 +0000

    task 1

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,9 +1,8 @@
-#include <stdio.h>
+#include <lists.h>
 
 /**
  * print_dlistint - print all the elements of a dlistint_t list
- * @h: pointer to the start of the linked list
- *
+ * @h: pointer to the linked list start
  * Return: number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,9 +1,8 @@
 #include "lists.h"
 
 /**
- * dlistint_len - count the number of nodes in the linked list
- * @h: pointer to the beginning of a linked list
- *
+ * dlistint_len - length of nodes in the linked list
+ * @h: pointer to the first element of a linked list
  * Return: number of nodes
  */
 size_t dlistint_len(const dlistint_t *h)
