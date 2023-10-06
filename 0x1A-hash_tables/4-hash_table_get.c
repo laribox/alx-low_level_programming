3999f894d7ce5ed0ae28a4cd33442e0624020b1d
laribox <laribox2014@gmail.com>
2023-10-07 00:14:05 +0100

    task 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - retrieves the value selected key
+ * @ht: hash table
+ * @key: the key
+ * Return: the value for the key or NULL
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *current;
+
+	if (!ht || !key || strcmp(key, "") == 0)
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	current = ht->array[index];
+
+	while (current)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			return (current->value);
+		}
+		current = current->next;
+	}
+	return (1);
+}
