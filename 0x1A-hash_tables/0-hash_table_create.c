6d066435cee1983a3b2465521585e76f2e2f5730
laribox <laribox2014@gmail.com>
2023-10-07 00:33:21 +0100

    fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,24 +7,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *res = malloc(sizeof(*res));
-	hash_node_t **arr;
-	unsigned long int i = 0;
 
-	if (!res)
-		return (0);
-	arr = malloc(size * sizeof(*arr));
-	if (!arr)
-	{
-		free(res);
-		return (0);
-	}
-	while (i < size)
-	{
-		arr[i] = NULL;
-		i++;
-	}
-	res->size = size;
-	res->array = arr;
 	return (res);
 }
 
